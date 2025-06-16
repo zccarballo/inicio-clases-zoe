1bbcbf4a2f33805133e72601db7c991e2412289d
zccarballo <zchacar.genius@gmail.com>
2025-06-16 10:23:14 +0200

    exercicio resolto

--- clases.js
+++ clases.js
@@ -3,21 +3,69 @@
  * class
  * new -> serve para solicitar a instancia dunha clase
  */
-export class Sumas{
 
-    sumaDousNumeros(a,b){
-        return a+b;
+//CLASES
+// export class Sumas{
+
+//     sumaDousNumeros(a,b){
+//         return a+b;
+//     }
+
+//     sumaTresNumeros(a,b,c){
+//         return a+b+c;
+//     }
+
+// }
+
+
+// export class Restas{
+//     restaDousNumeros(a,b){
+//         return a-b;
+//     }
+// }
+
+// export class Calculadora {
+//     suma(a, b){
+//         return a+b;
+//     }
+//     resta(a,b){
+//         return a-b;
+//     }
+//     multiplicacion(a,b){
+//         return a*b;
+//     }
+//     division(a,b){
+//         return a/b;
+//     }
+// }
+
+//CONSTRUCTOR
+export class Calculadora {
+  valor1;
+  valor2;
+
+  constructor(variable1, variable2) {
+    this.valor1 = variable1;
+    this.valor2 = variable2;
+    console.log('Instancia de clase Calculadora creada');
+  }
+
+  suma() {
+    return this.valor1 + this.valor2;
+  }
+
+  resta() {
+    return this.valor1 - this.valor2;
+  }
+
+  multiplicacion() {
+    return this.valor1 * this.valor2;
+  }
+
+  division() {
+    if (this.valor2 === 0) {
+      return 'Error: división por cero';
     }
-
-    sumaTresNumeros(a,b,c){
-        return a+b+c;
-    }
-
-}
-
-
-export class Restas{
-    restaDousNumeros(a,b){
-        return a-b;
-    }
+    return this.valor1 / this.valor2;
+  }
 }
--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-import {Sumas, Restas} from './clases.js';
-//####### INSTANCIAS DAS CLASSES #######
-let instanciaSumas = new Sumas(); //instancia a clase Sumas, mediante a palabra chave 'new'
-let instanciaRestas = new Restas(); //instancia a clase Restas, mediante a palabra chave 'new'
+// import {Sumas, Restas, Calculadora} from './clases.js';
+// //####### INSTANCIAS DAS CLASSES #######
+// let instanciaSumas = new Sumas(); //instancia a clase Sumas, mediante a palabra chave 'new'
+// let instanciaRestas = new Restas(); //instancia a clase Restas, mediante a palabra chave 'new'
+// let instanciaCalculadora = new Calculadora();
 
-//####### MÉTODOS DAS CLASSES #######
-let sumaDeDousNumeros = instanciaSumas.sumaDousNumeros(2,3); //chama a funcao sumaDousNumeros da instancia sumas
-let sumaDeTresNumeros = instanciaSumas.sumaTresNumeros(2,3,4); //chama a funcao sumaTresNumeros da instancia sumas
-let restaDousNumeros = instanciaRestas.restaDousNumeros(2,3); //chama a funcao restaDousNumeros da instancia restas
-//###### SAÍDAS POR CONSOLA
-console.log(sumaDeDousNumeros);
-console.log(sumaDeTresNumeros);
-console.log(restaDousNumeros);
+// //####### MÉTODOS DAS CLASSES #######
+// let sumaDeDousNumeros = instanciaSumas.sumaDousNumeros(2,3); //chama a funcao sumaDousNumeros da instancia sumas
+// let sumaDeTresNumeros = instanciaSumas.sumaTresNumeros(2,3,4); //chama a funcao sumaTresNumeros da instancia sumas
+// let restaDousNumeros = instanciaRestas.restaDousNumeros(2,3); //chama a funcao restaDousNumeros da instancia restas
+// let calculadora1 = instanciaCalculadora.suma(2,2);
+// let calculadora2 = instanciaCalculadora.resta(2,2);
+// let calculadora3 = instanciaCalculadora.multiplicacion(2,2);
+// let calculadora4 = instanciaCalculadora.division(2,2);
+// //###### SAÍDAS POR CONSOLA
+// console.log(sumaDeDousNumeros);
+// console.log(sumaDeTresNumeros);
+// console.log(restaDousNumeros);
+// console.log("suma de dous numeros: ", calculadora1);
+// console.log("resta de dous numeros: ", calculadora2);
+// console.log("multiplicacion de dous numeros: ", calculadora3);
+// console.log("division de dous numeros: ", calculadora4);
+
+import { Calculadora } from './clases.js';
+
+let calc = new Calculadora(10, 5);
+
+console.log('Suma:', calc.suma());                 
+console.log('Resta:', calc.resta());               
+console.log('Multiplicación:', calc.multiplicacion()); 
+console.log('División:', calc.division());         
